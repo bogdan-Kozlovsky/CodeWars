6ceb1034e53938547469464058d8927e688cab63
Bogdan <bogdankozlovski18@gmail.com>
2021-11-23 10:49:34 +0200

    validate code with simple regex

--- 8_kuy/Switch/CaseBugFixing#6.js
+++ 8_kuy/Switch/CaseBugFixing#6.js
@@ -5,19 +5,19 @@ Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluat
  */
 
 
-function evalObject(value){
-    var result = 0;
-    switch(value.operation){
-        case'+': result = value.a + value.b;
-            break
-        case'-': result = value.a - value.b;
-            break
-        case'/': result = value.a / value.b;
-            break
-        case'*': result = value.a * value.b;
-            break
-        case'%': result = value.a % value.b;
-            break
-        case'^': result = Math.pow(value.a, value.b);
-    }
-    return result;
+// function evalObject(value){
+//     var result = 0;
+//     switch(value.operation){
+//         case'+': result = value.a + value.b;
+//             break
+//         case'-': result = value.a - value.b;
+//             break
+//         case'/': result = value.a / value.b;
+//             break
+//         case'*': result = value.a * value.b;
+//             break
+//         case'%': result = value.a % value.b;
+//             break
+//         case'^': result = Math.pow(value.a, value.b);
+//     }
+//     return result;
