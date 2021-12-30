0e839a48faa382e03a1f2f91451538fe7fe2f4cd
Bogdan <bogdankozlovski18@gmail.com>
2021-12-30 19:39:36 +0200

    Beginner - Lost Without a Map

---
+++ 8_kuy/Beginner - Lost Without a Map.js
@@ -0,0 +1,11 @@
+/*
+Beginner - Lost Without a Map
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+ */
+
+function maps(x){
+    return x.map(e => e * 2)
+}
