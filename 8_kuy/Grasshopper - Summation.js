116dee2c9cd3daf26426e9ddcc8a8f9f34e8bb49
Bogdan <bogdankozlovski18@gmail.com>
2022-01-03 20:40:23 +0200

    Grasshopper - Summation

--- 8_kuy/Grasshopper - Summation.js
+++ 8_kuy/Grasshopper - Summation.js
@@ -6,4 +6,10 @@ Write a program that finds the summation of every number from 1 to num. The numb
 
 // const summation = n => (n * (n + 1)) / 2;
 
-const summation = n => (n * (n + 1))
+var summation = function (num) {
+    let a = 0
+    for(let i = 1; i <= num; i++){
+        a = a + i
+    }
+    return a
+}
