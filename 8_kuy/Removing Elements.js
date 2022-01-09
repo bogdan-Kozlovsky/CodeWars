c4996722617462ee818d00b53e8536990c2f1cbd
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-01-09 22:40:13 +0200

    Removing Elements

---
+++ 8_kuy/Removing Elements.js
@@ -0,0 +1,9 @@
+/*
+Removing Elements
+
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+ */
+
+function removeEveryOther(arr){
+    return arr.filter((f , i) => i % 2 === 0)
+}
