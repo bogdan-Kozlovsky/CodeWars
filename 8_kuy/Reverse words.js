044b1b40acd41d1e10dc63d3147ea33e6f200533
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-01-10 19:07:21 +0200

    Reverse words

---
+++ 8_kuy/Reverse words.js
@@ -0,0 +1,8 @@
+/*
+Reverse words
+
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+ */
+function reverseWords(str) {
+    return str.split(' ').map(m => m.split('')).map(m => m.reverse()).map(m => m.join('')).join(' ')
+}
