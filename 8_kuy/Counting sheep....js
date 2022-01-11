f93d797e6d7c8270b438e7bbd4dbd2f505e446d8
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-01-11 22:24:01 +0200

    Counting sheep...(refactor)

--- 8_kuy/Counting sheep....js
+++ 8_kuy/Counting sheep....js
@@ -16,14 +16,6 @@ For example,
 
 
 
-function countSheeps(arrayOfSheep) {
-    let sheepCounter = 0;
-    for( let i = 0; i < arrayOfSheep.length; i++) {
-        if(arrayOfSheep[i]=== true) {
-            sheepCounter++;
-        }
-    }
-    return sheepCounter;
-}
+const countSheeps = arrayOfSheep =>  arrayOfSheep.filter(f => f === true).length
 
 
