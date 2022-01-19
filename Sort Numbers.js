785280d805ccda335ddbe087ce8a296206e243d0
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-01-19 07:19:22 +0200

    Sort Numbers

---
+++ Sort Numbers.js
@@ -0,0 +1,16 @@
+/*
+Sort Numbers
+
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+ */
+
+function solution(nums){
+    if(nums === []){
+        return []
+    }else if(nums === null){
+        return []
+    }
+
+    let a = nums.sort((a , b ) => a - b)
+    return a
+}
