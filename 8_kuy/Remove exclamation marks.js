146668220808c06381ee9dbc0b3ccaa0c52fc0f6
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-01-23 20:11:58 +0200

    Remove exclamation marks

---
+++ 8_kuy/Remove exclamation marks.js
@@ -0,0 +1,7 @@
+/*
+Remove exclamation marks
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+ */
+
+const removeExclamationMarks = s => s.split('').filter(f => f !== "!").join('')
