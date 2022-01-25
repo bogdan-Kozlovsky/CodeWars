20a7c40b45ff64ff552905616e1e266c161b5f8c
bogdan-Kozlovsky <bogdankozlovsky18@gmail.com>
2022-01-25 19:10:58 +0200

    JavaScript Array Filter

---
+++ 8_kuy/JavaScript Array Filter.js
@@ -0,0 +1,11 @@
+/*
+JavaScript Array Filter
+
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+ */
+
+function getEvenNumbers(numbersArray){
+    return numbersArray.fitler(f => f % 2 === 0)
+}
