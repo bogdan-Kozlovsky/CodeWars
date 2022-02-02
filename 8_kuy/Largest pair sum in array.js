663dc4d7d088252492b285bde279762a945a2f03
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-02-02 12:51:53 +0200

    Palindrome Strings

---
+++ 8_kuy/Largest pair sum in array.js
@@ -0,0 +1,10 @@
+/*
+Largest pair sum in array
+Given a sequence of numbers, find the largest pair sum in the sequence.
+
+ */
+
+const largestPairSum = (numbers) => {
+    let a = numbers.sort((a,b) => a - b)
+    return a[a.length - 2] + a[a.length - 1]
+}
