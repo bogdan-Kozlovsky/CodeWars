bc8d8689f760e1ef425d64e2889018ccb8f06c4b
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-02-06 20:24:44 +0200

    Odd or Even?

---
+++ Odd or Even.js
@@ -0,0 +1,14 @@
+/*
+Odd or Even?
+
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+ */
+
+function oddOrEven(array) {
+    let a = array.map(i => b += i, b = 0 )
+    return b % 2 ? "odd" : "even"
+}
