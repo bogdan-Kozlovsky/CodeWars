a218a96495ced8d7ca6ab87181a98e710b7bfdb3
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-02-15 19:29:12 +0200

    Is it a palindrome?

---
+++ 8_kuy/Is it a palindrome.js
@@ -0,0 +1,12 @@
+/*
+Is it a palindrome?
+
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+
+ */
+
+function isPalindrome(x) {
+    x = x.toLowerCase()
+    return x.split('').reverse().join('') === x
+}
