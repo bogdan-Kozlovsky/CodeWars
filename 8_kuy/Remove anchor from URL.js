ba74c9f5eeb5edeac18658c2baa81a00b46acebd
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-02-20 21:14:38 +0200

    Remove anchor from URL

---
+++ 8_kuy/Remove anchor from URL.js
@@ -0,0 +1,7 @@
+/*
+Remove anchor from URL
+
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+ */
+
+const removeUrlAnchor = url => url.split('#')[0]
