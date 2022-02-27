26059699b92fdf6862959349618d8b7b8b15d70a
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-02-27 13:29:47 +0200

    V A P O R C O D E

---
+++ 8_kuy/V A P O R C O D E.js
@@ -0,0 +1,11 @@
+/*
+V A P O R C O D E
+
+Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+
+
+ */
+
+const vaporcode = string =>  {
+    return string.split('').map(e => e.toUpperCase()).filter(f => f !== ' ').join('  ')
+}
