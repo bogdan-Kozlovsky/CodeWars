c969cbecb3376d0201ac52db391aa7cd49d6daa3
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-03-18 20:38:14 +0200

    Count the divisors of a number

---
+++ 8_kuy/Count the divisors of a number.js
@@ -0,0 +1,15 @@
+/*
+Count the divisors of a number
+
+Count the number of divisors of a positive integer n.
+ */
+
+function getDivisorsCnt(n){
+    let count = 0
+    for(let i = 1; i <= n; i++){
+        if(n % i === 0){
+            count++
+        }
+    }
+    return count
+}
