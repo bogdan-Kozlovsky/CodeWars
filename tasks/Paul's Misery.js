dc5be6cca886ebe899a488b2470fb173ec355611
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-05-03 21:20:16 +0300

    Paul's Misery

---
+++ tasks/Paul's Misery.js
@@ -0,0 +1,16 @@
+/*
+Paul's Misery
+
+Paul is an excellent coder and sits high on the CW leaderboard. He solves kata like a banshee but would also like to lead a normal life, with other activities. But he just can't stop solving all the kata!!
+
+Given an array (x) you need to calculate the Paul Misery Score. The values are worth the following points:
+ */
+
+function paul(x){
+    let n = 0;
+    x.map(v => v === 'Petes kata'? n += 10 : v === 'kata' ? n +=5 : v === 'eating' ? n += 1 : null);
+    if (n < 40) return 'Super happy!';
+    if (n >= 40 && n < 70) return 'Happy!';
+    if (n >= 70 && n < 100) return 'Sad!';
+    if (n >= 100) return 'Miserable!';
+}
