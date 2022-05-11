940cc6d6a86f4799b80cf57dacf0d30778ae194c
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-05-11 10:56:37 +0300

    Semi-Optional

---
+++ tasks/Semi-Optional.js
@@ -0,0 +1,8 @@
+/*
+Semi-Optional
+
+We have implemented a function wrap(value) that takes a value of arbitrary type and wraps it in a new JavaScript Object or Python Dict setting the 'value' key on the new Object or Dict to the passed-in value.
+
+ */
+
+const wrap = value =>  ({value})
