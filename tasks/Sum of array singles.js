7b4434b6438b0e9683070d84c6439aa61bee36a6
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-05-14 21:42:01 +0300

    Sum of array singles

---
+++ tasks/Sum of array singles.js
@@ -0,0 +1,14 @@
+/*
+Sum of array singles
+
+In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+More examples in the test cases.
+ */
+
+const repeats = array =>
+    array
+        .filter(number => array.indexOf(number) === array.lastIndexOf(number))
+        .reduce((total, number) => total + number, 0)
