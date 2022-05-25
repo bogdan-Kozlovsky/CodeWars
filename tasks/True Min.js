18fad3709d438733ea862b87fd64d7ad1fe159f3
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-05-25 18:26:45 +0300

    True Min

---
+++ tasks/True Min.js
@@ -0,0 +1,16 @@
+/*
+True Min
+
+The Math.min function has stopped working, so we have to use our own function. We are off to a good start, but this function doesn't seem to handle everything properly. Add in the proper checks to return NaN for anything that isn't an actual number, except treat null like 0.
+
+Note: This min function need not handle more than two arguments.
+ */
+
+const min = (a , b) => {
+    a === null && (a = 0);
+    b === null && (b = 0);
+    if(isNaN(a) || isNaN(b)){
+        return NaN
+    }
+    return a < b ? a : b
+}
