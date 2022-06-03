69e3d788559b18d4c72d728c8029f5ae5e5333d1
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-06-03 11:26:30 +0300

    Sort the Gift Code

---
+++ tasks/Sort the Gift Code.js
@@ -0,0 +1,9 @@
+/*
+Sort the Gift Code
+
+Santa's senior gift organizer Elf developed a way to represent up to 26 gifts by assigning a unique alphabetical character to each gift. After each gift was assigned a character, the gift organizer Elf then joined the characters to form the gift ordering code.
+
+Santa asked his organizer to order the characters in alphabetical order, but the Elf fell asleep from consuming too much hot chocolate and candy canes! Can you help him out?
+ */
+
+const sortGiftCode = code => code.split('').sort().join('');
