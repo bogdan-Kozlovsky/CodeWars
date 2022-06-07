5b65e960109dd1eb7602bb2407045ca6dda8154f
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-06-07 19:32:23 +0300

    Welcome!

---
+++ tasks/Welcome!.js
@@ -0,0 +1,38 @@
+/*
+
+Welcome!
+
+Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+ */
+
+function greet(language) {
+  const data = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+
+
+  for (key in data) {
+
+    if (key === language) {
+      return  data[key]
+    }
+
+  }
+  return 'Welcome'
+}
