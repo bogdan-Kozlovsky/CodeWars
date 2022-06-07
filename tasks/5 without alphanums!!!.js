41bc8dac60421ea606209b916b4f496a780c9c81
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-06-07 16:17:07 +0300

    5 without alphanums!!!

---
+++ tasks/5 without alphanums!!!.js
@@ -0,0 +1,10 @@
+/*
+
+5 without alphanums!!!
+
+Write a function/proc/lambda called _ that always returns 5.
+
+Sounds easy right? Just bear in mind that you can only use the following characters:
+ */
+
+_=_=>-~-~-~-~-~[]
