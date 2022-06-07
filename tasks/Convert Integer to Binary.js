8f40aec2bb35f6064bd187df19ab6d57820db2a1
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-06-07 22:00:31 +0300

    Convert Integer to Binary

---
+++ tasks/Convert Integer to Binary.js
@@ -0,0 +1,8 @@
+/*
+Convert Integer to Binary
+
+Convert integers to binary as simple as that. You would be given an integer as a argument and you have to return its binary form. To get an idea about how to convert a decimal number into a binary number, visit here.
+
+ */
+
+const toBinary = (n) => (n >>> 0).toString(2);
