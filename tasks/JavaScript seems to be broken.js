b8382aa7ffedef3fe38e89a3ed03a450e72d7f72
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-06-11 16:18:03 +0300

    JavaScript seems to be broken

---
+++ tasks/JavaScript seems to be broken.js
@@ -0,0 +1,14 @@
+/*
+JavaScript seems to be broken
+
+There is some horribly rotten script that is not required in your project anymore, and it is affecting the way your code is supposed to work.
+
+For some reason you don't have an access to that rotten piece of code but you want to get your code working as expected in any possible way.
+
+Go and get it working!
+ */
+
+function semicolonSeparationToCommaSeparation(input) {
+  let [a, b, c] = input.split(';');
+  return `${a},${b},${c}`;
+}
