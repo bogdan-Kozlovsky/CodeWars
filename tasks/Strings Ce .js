ffd0da7ed9b7a9ed4570a6351b1e0c3dea16ee01
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-06-26 21:01:16 +0300

    Ce*s*r*d Strings

---
+++ tasks/Strings Ce .js
@@ -0,0 +1,17 @@
+/*
+Ce*s*r*d Strings
+
+My PC got infected by a strange virus. It only infects my text files and replaces random letters by *, li*e th*s (like this).
+
+Fortunately, I discovered that the virus hides my censored letters inside root directory.
+
+It will be very tedious to recover all these files manually, so your goal is to implement uncensor function that does the hard work automatically.
+ */
+
+function uncensor(infected, discovered) {
+  for(let i = 0; i < discovered.length; i++){
+    infected = infected.replace(/\*/, discovered[i] )
+  }
+
+  return infected
+}
