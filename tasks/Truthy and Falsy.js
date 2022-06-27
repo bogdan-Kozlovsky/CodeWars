b0b600547ebb408efb36a60552f6e12011694892
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-06-27 10:53:00 +0300

    Truthy and Falsy

---
+++ tasks/Truthy and Falsy.js
@@ -0,0 +1,9 @@
+/*
+
+Truthy and Falsy
+
+You are given two empty arrays (truthy and falsy) and you have to fill this array with at least 5 elements in each which will evaluate to true or false accordingly.
+ */
+
+const truthy = [1,[],{},'1',true];
+const falsy = [0,false,'',null,undefined];
