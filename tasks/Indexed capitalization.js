6356b3e3305e9c75c51ad26c4c8c78880d83c0a8
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-07-03 10:03:59 +0300

    Indexed capitalization

---
+++ tasks/Indexed capitalization.js
@@ -0,0 +1,14 @@
+/*
+Indexed capitalization
+
+Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+For example:
+
+capitalize("abcdef",[1,2,5]) = "aBCdeF"
+capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+The input will be a lowercase string with no spaces and an array of digits.
+
+ */
+
+const capitalize = (s,arr) => s.split('').map((item , index) => arr.includes(index) ? item.toUpperCase() : item).join('');
