6ce20a8c7b9540cc1114375531a3bfe2d69008d5
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-07-09 08:49:03 +0300

    Inspiring Strings

---
+++ tasks/Inspiring Strings.js
@@ -0,0 +1,7 @@
+/*
+Inspiring Strings
+
+When given a string of space separated words, return the word with the longest length. If there are multiple words with the longest length, return the last instance of the word with the longest length.
+ */
+
+const longestWord = stringOfWords =>  stringOfWords.split(' ').sort((a,b) => a.length - b.length).slice(-1).join('');
