4e8d88a68fed2417e0a5375f701ecc6ad8b080ba
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-07-17 17:17:16 +0300

    Return the closest number multiple of 10

---
+++ tasks/Return the closest number multiple of 10.js
@@ -0,0 +1,7 @@
+/*
+Return the closest number multiple of 10
+
+Given a number return the closest number to it that is divisible by 10.
+ */
+
+const closestMultiple10 = num => Math.round(num / 10) * 10;
