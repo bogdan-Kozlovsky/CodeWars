3c42069971a8005d901b917ddb5f67a29e4c1060
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-08-02 15:33:32 +0300

    Triple trouble

---
+++ tasks/Triple trouble.js
@@ -0,0 +1,16 @@
+/*
+Triple trouble
+
+which takes numbers num1 and num2 and returns 1 if there is a straight triple of a number at any place in num1 and also a straight double of the same number in num2.
+
+If this isn't the case, return 0
+ */
+
+const tripledouble = (num1, num2) => {
+  for (let i = 0; i < 10; i++) {
+    if (new RegExp(`${i}{3}`).test(num1) && new RegExp(`${i}{2}`).test(num2)) {
+      return 1;
+    }
+  }
+  return 0;
+}
