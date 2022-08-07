e4dc30901e1eb9024476d1e2df8c44a50bb3d614
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-08-07 20:36:27 +0300

    The Office III - Broken Photocopier

---
+++ tasks/The Office III - Broken Photocopier.js
@@ -0,0 +1,13 @@
+/*
+The Office III - Broken Photocopier
+
+The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+
+Given a string of binary, return the version the photocopier gives you as a string.
+ */
+
+const broken = x => {
+    return x.split('').map(el => el === '1' ? '0' : '1').join('')
+}
