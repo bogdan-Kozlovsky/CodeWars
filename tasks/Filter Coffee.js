95a8747eba38beb9fb54d4ea8325d171df8609c3
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-11-03 21:39:13 +0200

    Filter Coffee

---
+++ tasks/Filter Coffee.js
@@ -0,0 +1,11 @@
+/*
+Filter Coffee
+
+https://www.codewars.com/kata/56069d0c4af7f633910000d3/train/javascript
+ */
+
+function search(budget, prices) {
+    return prices.filter(el => el <= budget).sort((a, b) => a - b).join(',');
+}
+
+console.log(search(14, [7, 3, 23, 9, 14, 20, 7]));
