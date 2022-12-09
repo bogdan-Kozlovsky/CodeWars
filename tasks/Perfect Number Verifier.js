7bba540aaac3958b7600eb5a0359b3d351a56d48
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-12-09 22:45:07 +0200

    Perfect Number Verifier

---
+++ tasks/Perfect Number Verifier.js
@@ -0,0 +1,9 @@
+/*
+Perfect Number Verifier
+
+https://www.codewars.com/kata/56a28c30d7eb6acef700004d/train/javascript
+ */
+
+function isPerfect(n) {
+    return [6, 28, 496, 8128, 33550336, 8589869056, 137438691328].includes(n);
+}
